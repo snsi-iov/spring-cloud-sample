20a1ae3048985d5d9b693371f9b7f390e4f2b192
minglungweng <minglungweng@iii.org.tw>
2016-08-24 17:21:00 +0800

    MODIFY: 加入測試範例, 修改設定檔

---
+++ demo-microservice/src/test/java/run/ApplicationTest.java
@@ -0,0 +1,42 @@
+package run;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.context.ConfigurableApplicationContext;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//@PropertySource({"classpath:bar-development.properties", "classpath:bar-production.properties"}) //this properties should get from config server at runtime
+public class ApplicationTest {
+
+    private static ConfigurableApplicationContext server;
+
+    private static String serverPort;
+	
+	@BeforeClass
+	public static void startServer() throws Exception {
+		server = SpringApplication.run(Application.class, "--server.port=0",   //use random port to avoid port-confliction 
+										"--spring.profiles.active=development");  //active profile 'development'
+		serverPort = server.getEnvironment().getProperty("local.server.port");
+	}
+	
+    @AfterClass
+    public static void close() throws IOException {
+        if (server != null) {
+            server.close();
+        }
+    }
+    
+	@Test
+	public void queryTest() {
+		String response = new TestRestTemplate().getForObject("http://localhost:"+serverPort+"/?q=test.hellow", String.class);
+		Assert.assertEquals("yoooooooooooooo development", response);
+	}
+
+}
